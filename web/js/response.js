5a0e8468e528cad14eeba75e803846190019f63a
Jeffrey Dungen <jeff@reelyactive.com>
2015-08-12 23:11:26 -0400

    Standardised with Angular style guide

--- web/js/response.js
+++ web/js/response.js
@@ -1,6 +1,6 @@
-REFRESH_MILLISECONDS = 2000;
+DEFAULT_POLLING_MILLISECONDS = 2000;
 
-angular.module('response', [])
+angular.module('response', [ 'ui.bootstrap' ])
 
   // API controller
   .controller('ApiCtrl', function($scope, $http, $interval, $window) {
@@ -8,8 +8,19 @@ angular.module('response', [])
     $scope.meta = { message: "loading", statusCode: "..." };
     $scope.links = { self: { href: url } };
     $scope.devices = {};
+    $scope.metadata = {};
+    $scope.expand = true;
 
-    updateQuery();
+    function attachMetadata() {
+      for(var id in $scope.devices) {
+        if ($scope.devices.hasOwnProperty(id)) {
+          var device = $scope.devices[id];
+          if(typeof($scope.metadata[id]) != 'undefined') {
+            device.metadata = JSON.stringify($scope.metadata[id], null, "  ");
+          }
+        }
+      }
+    }
 
     function updateQuery() {
       $http.defaults.headers.common.Accept = 'application/json';
@@ -18,6 +29,7 @@ angular.module('response', [])
           $scope.meta = data._meta;
           $scope.links = data._links;
           $scope.devices = data.devices;
+          attachMetadata();
         })
         .error(function(data, status, headers, config) {
           $scope.meta = data._meta;
@@ -25,5 +37,42 @@ angular.module('response', [])
           $scope.devices = {};
         });
     }
-    $interval(updateQuery, REFRESH_MILLISECONDS);
+
+    $scope.fetch = function (id) {
+      if((typeof($scope.devices[id]) != 'undefined') &&
+         (typeof($scope.devices[id].url) != 'undefined')) {
+        $http.defaults.headers.common.Accept = 'application/json';
+        $http.get($scope.devices[id].url)
+          .success(function(data, status, headers, config) {
+            $scope.metadata[id] = data;
+            attachMetadata();
+          })
+          .error(function(data, status, headers, config) {
+            $scope.metadata[id] = { error: error };
+          });
+      }
+      else {
+        $scope.metadata[id] = { error: "No URL to fetch" };
+      }
+    };
+
+    $scope.isEmpty = function () {
+      return (Object.keys($scope.devices).length === 0);
+    };
+
+    $scope.updatePeriod = function (period) {
+      if(period) {
+        $scope.pollingMessage = "Polling every " + (period / 1000) + "s";
+        $interval.cancel($scope.pollingPromise);
+        $scope.pollingPromise = $interval(updateQuery, period);
+        updateQuery();
+      }
+      else {
+        $scope.pollingMessage = "Polling disabled";
+        $interval.cancel($scope.pollingPromise);
+      }
+    };
+
+    updateQuery();
+    $scope.updatePeriod(DEFAULT_POLLING_MILLISECONDS);
   });
